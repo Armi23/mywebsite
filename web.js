dd224d40782e86ce260eaedc2706c2f06d7aacb5
Armi23 <armaghan23@gmail.com>
2013-08-29 19:10:58 +0000

    changed public to libs

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
     response.send(buf.toString('utf-8'));
 });
 
-app.use("/public",express.static(path.join(__dirname, '/public')));
+app.use("/public",express.static(path.join(__dirname, '/libs')));
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
